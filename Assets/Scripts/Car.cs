4cefcf228d51eee8cfa56192518375786e8e0de6
siaii <terence.andre@yahoo.com>
2022-02-13 03:46:08 +0800

    Implemented auto car spawn and car stopping

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    [Header("Fields")]
+    [SerializeField] private float moveSpeed = 10;
+    [SerializeField] private bool enableMovement = true;
+    [SerializeField] private LayerMask mask;
+    
+    [Header("Car Stopping Distance")]
+    [SerializeField] private Vector3 stoppingCenter;
+    [SerializeField] private Vector3 stoppingHalfExtents;
+    
+    private bool isMoving = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,29 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * (10 * Time.deltaTime));
+        //Prevent cars from colliding with the player and each others
+        CheckFrontCollision();
+        if (enableMovement && isMoving)
+        {
+            transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));    
+        }
+    }
+
+    void CheckFrontCollision()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Default");
+        var collision = Physics.OverlapBox(transform.position + transform.TransformDirection(stoppingCenter), stoppingHalfExtents, Quaternion.identity, mask.value);
+        
+        isMoving = collision.Length <= 0;
+        
+        gameObject.layer = LayerMask.NameToLayer("Car");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DestroyCar"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    [SerializeField] private Car[] carsPrefabs;
-    [SerializeField] private float TimeBetweenSpawn;
+    //Spawning bool controlled via editor
+    [SerializeField] private bool EnableSpawning = true;
+    [SerializeField] private Car[] CarsPrefabs;
+    [SerializeField] private float TimeBetweenSpawn = 1f;
     [SerializeField] private Vector3 RoadDirection;
-    
+
+    private float timer = 0;
+    //Spawning bool controlled via code
+    private bool isSpawningCar;
+    private int mask;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Only check for cars and player
+        mask = LayerMask.GetMask("Car", "Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        //Make sure nom cars are occupying the spawn space
+        CheckSpawnValid();
+        if (EnableSpawning && isSpawningCar && timer>=TimeBetweenSpawn)
+        {
+            SpawnCar();
+            timer = 0;
+        }
     }
 
+    //Implement object pooling if have enough time and/or performance issues
     public void SpawnCar()
     {
-        var prefab = carsPrefabs[Random.Range(0, carsPrefabs.Length)].gameObject;
+        var prefab = CarsPrefabs[Random.Range(0, CarsPrefabs.Length)].gameObject;
         var instance = Instantiate(prefab, transform.position, Quaternion.LookRotation(RoadDirection));
+    }
+
+    private void CheckSpawnValid()
+    {
+        var collision = Physics.OverlapBox(transform.position , Vector3.one*2, Quaternion.identity, mask);
+        isSpawningCar = collision.Length <= 0;
     }
 }
